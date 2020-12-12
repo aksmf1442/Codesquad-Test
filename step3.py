20e2ddc37a508c30ec3e9637e62210b9eda3daf7
aksmf1442 <64648893+aksmf1442@users.noreply.github.com>
2020-12-12 19:34:00 +0900

    Step3) 큐브 돌리는 방식 선택 기능(랜덤, 선택), 큐브현재상태와  결과 출력 함수 추가
    1. chooseRandomInput함수는 큐브를 랜덤으로 돌릴건지 직접 입력받아서 돌릴건지를 선택할 수 있게 해준다.
    2. randomInput함수는 큐브 돌리는 방향을 하나 랜덤으로 골라서 큐브를 돌릴 수 있게 한다.
    3. selectInput함수는 큐브 돌릴 방향을 입력받아서 그에 맞게 돌릴 수 있게 한다.
    4. printResult함수는 결과를 출력할 수 있게해준다.
    5. stateCube함수는 현재 큐브의 상태를 출력해준다.

--- step3.py
+++ step3.py
@@ -1,3 +1,88 @@
+import copy, time, datetime, random
+
+def modifyDirection(direction):
+    pass
+
+def filterDirection(direction):
+    pass
+
+def classifyDirection(cube, direction, count):
+    pass
+
+def checkCube(cube):
+    pass
+
+
+
+# 현재 큐브 상태 출력
+def stateCube(cube):
+    [print("\t",' '.join(i)) for i in cube[0]]
+    print()
+
+    for j in range(3):
+        for idx in range(1, 5):
+            print(' '.join(cube[idx][j]), end="    ")
+        print()
+    print()
+
+    [print("\t",' '.join(i)) for i in cube[5]]
+    print()    
+
+# 변환된 cube, count값을 받아오고 direction을 입력받는 함수 (Q 입력시 종료)
+def selectInput(cube):
+    count = 0
+    while True:
+        direction = input("CUBE> ").upper()
+        if direction == "Q":
+            return count
+        # direction에서 '가 포함된 값을 소문자로 변환
+        direction = modifyDirection(direction)
+        # direction을 숫자포함해서 리스트 형태로 변환
+        direction = filterDirection(direction)
+        cube, count = classifyDirection(cube, direction, count)
+        if count >= 1 and checkCube(cube):
+            return count
+
+# 방향중 하나를 랜덤으로 골라서 큐브를 돌려주는 함수
+def randomInput(cube):
+    count = 0
+    lst = ["U","U2","U'","D","D2","D'","F","F2","F'","B","B2","B'","R","R2","R'","L","L2","L'"]
+    while True:
+        # lst에서 랜덤으로 값 하나 추출
+        direction = random.choice(lst)
+        # direction에서 '가 포함된 값을 소문자로 변환
+        direction = modifyDirection(direction)
+        # direction을 숫자포함해서 리스트 형태로 변환
+        direction = filterDirection(direction)
+        cube, count = classifyDirection(cube, direction, count)
+        if checkCube(cube):
+            return count
+
+# 결과출력함수
+def printResult(count, start):
+    seconds = time.time()- start
+    minutes = str(datetime.timedelta(seconds=seconds)).split(".")[0][2:]
+    print("경과시간 : {}".format(minutes))
+    print("조작갯수: {}".format(count))
+    print("이용해주셔서 감사합니다.😆")
+
+# 무작위로 섞을건지 직접 섞을건지 고르는 함수
+def chooseRandomInput(cube, start):
+    random = input("-둘 중 하나를 입력해주세요- || 무작위 섞기 : 1, 직접 입력받고 섞기 : 2 -> ")
+    # 큐브 초기 상태 출력
+    stateCube(cube)
+    
+    if random == "1":
+        count = randomInput(cube)
+        printResult(count, start)
+    elif random == "2":
+        print("Q입력시 프로그램이 종료됩니다!")
+        count = selectInput(cube)
+        printResult(count, start)
+    else:
+        print("잘못된 입력입니다. 1 혹은 2를 다시 입력해주세요!")
+        main()
+
 # 메인함수
 def main():
     # 시작 시간
